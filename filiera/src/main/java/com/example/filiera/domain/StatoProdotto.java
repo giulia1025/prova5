59667a8ffd5bc9f1486fcf6c4235c1a2b71970c4
giulia1025 <glupi1025@gmail.com>
2025-08-07 15:40:46 +0200

    Add files via upload

---
+++ filiera/src/main/java/com/example/filiera/domain/StatoProdotto.java
@@ -0,0 +1,3 @@
+
+package com.example.filiera.domain;
+public enum StatoProdotto { IN_ATTESA_APPROVAZIONE }
---
+++ filiera/src/main/java/com/example/filiera/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+
+package com.example.filiera.repository;
+import com.example.filiera.domain.Prodotto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+public interface ProductRepository extends JpaRepository<Prodotto, Long> {}
